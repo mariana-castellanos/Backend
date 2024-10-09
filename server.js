0807d5fbef31171f24e178b64234a3887af3bd99
nsochoaga <nsochoaga@unal.edu.co>
2024-10-08 22:13:36 -0500

    add cors

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+  }));
 app.use(express.json());
+
 app.use('/api/v1/inventario', inventarioRoutes);
 
 
